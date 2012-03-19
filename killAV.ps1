481a68103a640003212fe0ea183265bb8a9cf4e6
Jasmine English <jengl003@fiu.edu>
2012-03-18 22:21:32 -0400

    Simplifying removal of white space and extensions in module names... Trying to find a way to discover processes related to services so as to avoid this convoluted and possibly dangerous method.

--- killAV.ps1
+++ killAV.ps1
@@ -1,11 +1,27 @@
-﻿#--------------------------------------------------------------------------------------------
+﻿<# 
+.SYNOPSIS 
+    Stop all Anti-Virus processes and services, optionally disabling the services.
+.DESCRIPTION 
+    For those times when you need to temporarily stop Anti-Virus programs in order
+	to complete other tasks, such as installing software. This script filters
+	running processes and services using a master list of Anti-Virus companies.
+	Optionally, you can choose to disable the filtered services.
+.NOTES 
+    File Name  : killAV.ps1 
+    Author     : Jasmine English - jengl003@fiu.edu 
+    Requires   : PowerShell V2, AVListMaster.txt
+.LINK 
+    https://github.com/theRedmage/ITAuto-KillAV
+.EXAMPLE 
+    C:\PS>killAV.ps1
+    Starting Kill AV Script...
+	
+	Finished!
+#>
+
+#--------------------------------------------------------------------------------------------
 # STOP ANTI-VIRUS PROCESSES & SERVICES
 #--------------------------------------------------------------------------------------------
-# AUTHOR: Jasmine English
-# CONTACT: jengl003@fiu.edu
-# DATE CREATED: Dec. 5, 2011
-# DATE MODIFIED: March 18, 2012
-# ATTACHED: AVListMaster.txt, Instructions.txt
 # SOURCES:
 # 1 - Ed Wilson and Craig Liebendorfer, Scripting Guys
 # 	http://blogs.technet.com/b/heyscriptingguy/archive/2009/10/01/hey-scripting-guy-october-1-2009.aspx
@@ -22,7 +38,7 @@ Write-Host -ForegroundColor Blue "Starting Kill AV Script..."
 $ErrorActionPreference = "SilentlyContinue"
 
 $WorkDir = "C:\edu.fiu.jengl003\monitorAV\"					# Directory of script
-$AVListExclude = "Microsoft Corporation"					# List of AV companies to exclude
+#$AVListExclude = "Microsoft Corporation"					# List of AV companies to exclude
 $DisableAVService = $false									# Disable AV services to stop them
 															# from loading on reboot
 
@@ -60,7 +76,7 @@ $AVServices = @()
 	$temp2 = @()
 	
 	#Get services with same name as process
-	Get-Service -Name $AVProcs | `
+	Get-Service -Name $AVProcs |
 	ForEach-Object {
 		$temp0 += $_.Name
 	}
@@ -68,97 +84,97 @@ $AVServices = @()
 	#Get modules associated with a process, assuming some are services
 	foreach ($AVProc in $AVProcs) 
 	{
-		(Get-Process $AVProc).modules | Select-Object -Property modulename |`
-		Out-String -Stream | Select-String ".dll" -OutVariable temp1
+		$temp1 = (Get-Process $AVProc).modules | Select-Object -Property modulename |
+		Out-String -Stream | Select-String ".dll" #-OutVariable temp1
 	}
 	
 	#Format module name so that it can be called
 	foreach ($t1 in $temp1)
 	{
-		$t1.toString().trim() | Out-String -OutVariable t2 		 #Remove whitespace
-		$t2 -replace ".dll", "" | Out-String -OutVariable +temp2 #Remove extensions
+		$t2 = $t1.toString() -replace "\s", "" #| Out-String -OutVariable t2 		 #Remove whitespace
+		$temp2 += $t2 -replace ".dll", "" #| Out-String -OutVariable +temp2 #Remove extensions
 	}
 	
 	$AVServices = $temp0 + $temp2
 #}
 
-#Stop AV service (Modified from Source (1))
-#function Stop_AVService
+##Stop AV service (Modified from Source (1))
+##function Stop_AVService
+##{
+#	foreach ($Service in $AVServices)
+#	{
+#		#Check if a service, else, go to next item in list
+#		if ( !(Get-Service $Service) ) {continue}
+#		
+#		$Status = $Service.Status
+#		
+#		Write-Host -ForegroundColor DarkBlue "Service $Service status is $Status"
+#		$objWmiService = Get-Wmiobject -Class Win32_Service -Filter "name='$Service'"
+#		
+#		if ($objWMIService.Acceptstop)
+# 		{
+#			Write-Host -ForegroundColor DarkBlue "Stopping the $Service service now ..." 
+#			$rtn = $objWMIService.stopService()
+#			Switch ($rtn.returnvalue) 
+#  			{ 
+#			   0 {Write-Host -foregroundcolor green "Service $Service stopped"}
+#			   2 {Write-Host -foregroundcolor red "Service $Service reports access denied"}
+#			   5 {Write-Host -ForegroundColor red "Service $Service cannot accept control at this time"}
+#			   10 {Write-Host -ForegroundColor red "Service $Service is already stopped"}
+#			   DEFAULT {Write-Host -ForegroundColor red "Service $Service service reports ERROR $($rtn.returnValue)"}
+#  			}
+# 		}
+#		else
+# 		{ 
+#  			Write-Host -ForegroundColor magenta "$Service will not accept a stop request"
+# 		}
+# 	}
+##}
+#
+##Stop AV process (Modified from Sources (2), (3))
+##function Stop_AVProcess 
+##{
+#	foreach ($Process in $AVProcs)
+#	{
+#		$WMIProc = Get-WMIObject Win32_Process -Filter "name='$Process.exe'"
+#		#$Name = $WMIProc.Name
+#		$ID = $WMIProc.ProcessID
+#		
+#		$WMIProc | ForEach-Object {
+#			$rtn = $WMIProc.Terminate()
+#			switch ($rtn.ReturnValue)
+#			{
+#				0 {Write-Host -foregroundcolor green "Process $Process stopped"}
+#				2 {Write-Host -foregroundcolor red "Process $Process reports access denied"}
+#				3 {Write-Host -foregroundcolor red "Process $Process reports insufficient privilege"}
+#				8 {Write-Host -foregroundcolor red "Process $Process reports unknown failure"}
+#				9 {Write-Host -foregroundcolor red "Path not found for $Process"}
+#				21 {Write-Host -foregroundcolor red "Invalid WMI parameter"}
+#			}
+#		}
+#	}
+##}
+#
+##--------------------------------------------------------------------------------------------
+## Optional Functions
+##--------------------------------------------------------------------------------------------
+#
+##Prevent stopped AV service from starting up again after system restart
+##function Disable_AVService
+##{
+#if ($DisableAVService)
 #{
-	foreach ($Service in $AVServices)
-	{
-		#Check if a service, else, go to next item in list
-		if ( !(Get-Service $Service) ) {continue}
-		
-		$Status = $Service.Status
-		
-		Write-Host "Service $Service status is: $Status"
-		$objWmiService = Get-Wmiobject -Class Win32_Service -Filter "name='$Service'"
-		
-		if ($objWMIService.Acceptstop)
- 		{
-			Write-Host "Stopping the $Service service now ..." 
-			$rtn = $objWMIService.stopService()
-			Switch ($rtn.returnvalue) 
-  			{ 
-			   0 {Write-Host -foregroundcolor green "Service $Service stopped"}
-			   2 {Write-Host -foregroundcolor red "Service $Service reports access denied"}
-			   5 {Write-Host -ForegroundColor red "Service $Service cannot accept control at this time"}
-			   10 {Write-Host -ForegroundColor red "Service $Service is already stopped"}
-			   DEFAULT {Write-Host -ForegroundColor red "Service $Service service reports ERROR $($rtn.returnValue)"}
-  			}
- 		}
-		else
- 		{ 
-  			Write-Host -ForegroundColor magenta "$Service will not accept a stop request"
- 		}
- 	}
+#		foreach ($Service in $AVServices)
+#		{
+#			#Check if a service, else, go to next item in list
+#			if ( !(Get-Service $Service) ) {continue}
+#			
+#			Set-Service -Name $Service -StartupType Disabled
+#			
+#			Write-Host -foregroundcolor green "Service $Service disabled."
+#		}
 #}
-
-#Stop AV process (Modified from Sources (2), (3))
-#function Stop_AVProcess 
-#{
-	foreach ($Process in $AVProcs)
-	{
-		$WMIProc = Get-WMIObject Win32_Process -Filter "name='$Process.exe'"
-		#$Name = $WMIProc.Name
-		$ID = $WMIProc.ProcessID
-		
-		$WMIProc | ForEach-Object {
-			$rtn = $WMIProc.Terminate()
-			switch ($rtn.ReturnValue)
-			{
-				0 {Write-Host -foregroundcolor green "Process $Process stopped"}
-				2 {Write-Host -foregroundcolor red "Process $Process reports access denied"}
-				3 {Write-Host -foregroundcolor red "Process $Process reports insufficient privilege"}
-				8 {Write-Host -foregroundcolor red "Process $Process reports unknown failure"}
-				9 {Write-Host -foregroundcolor red "Path not found for $Process"}
-				21 {Write-Host -foregroundcolor red "Invalid WMI parameter"}
-			}
-		}
-	}
-#}
-
-#--------------------------------------------------------------------------------------------
-# Optional Functions
-#--------------------------------------------------------------------------------------------
-
-#Prevent stopped AV service from starting up again after system restart
-#function Disable_AVService
-#{
-if ($DisableAVService)
-{
-		foreach ($Service in $AVServices)
-		{
-			#Check if a service, else, go to next item in list
-			if ( !(Get-Service $Service) ) {continue}
-			
-			Set-Service -Name $Service -StartupType Disabled
-			
-			Write-Host -foregroundcolor green "Service $Service disabled."
-		}
-}
-#}
+##}
 
 Write-Host -ForegroundColor Blue "Finished!"
 #--------------------------------------------------------------------------------------------
